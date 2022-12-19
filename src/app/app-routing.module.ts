a2c5745637a3db4ea5a04ec31c590f6999caf49d
Shahidh Ahamad <syed2804shahid@gmail.com>
2022-12-19 16:35:41 +0530

    portfolio implementation

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'about' , component: AboutComponent},
+  {path:'education', component:EducationComponent},
+  {path:'skills',  component:SkillsComponent},
+  {path:'contact',component: ContactComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
